354ef37eb19e27e1c430389f212c4f298e10f4f5
KaroDev <karodev3@gmail.com>
2019-09-03 15:57:18 -0500

    Jump Script, Layer 'Ground' and new Collider sizes

---
+++ SpaceMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+    public LayerMask groundMask;
+
+    void Awake() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+    }
+
+    void Jump() {
+        if (IsTouchingTheGround()) {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        
+    }
+
+    //Nos indica si el personaje está o no tocando el suelo
+    bool IsTouchingTheGround() {
+        if (Physics2D.Raycast(this.transform.position, 
+                              Vector2.down,
+                              1f, //0.2 = cm
+                              groundMask)) {
+            //TODO: programar lógica de contacto con el suelo
+            return true;
+        } else {
+            //TODO: programar lógica de no contacto
+            return false;
+        }
+    }
+}
