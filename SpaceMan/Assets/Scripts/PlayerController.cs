8caef0e629ab04e8fd76396060011bd5ce9b89fc
KaroDev <karodev3@gmail.com>
2019-09-16 18:13:25 -0500

    New jump animations

--- SpaceMan/Assets/Scripts/PlayerController.cs
+++ SpaceMan/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,30 @@ public class PlayerController : MonoBehaviour
     //Variables del movimiento del personaje
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+    Animator animator;
     public LayerMask groundMask;
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+    private const string VERTICAL_FORCE = "verticalForce";
 
     void Awake() {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start() {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, false);
     }
 
     void Update() {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+        Debug.DrawRay(this.transform.position, Vector2.down*1f, Color.green);
+
+        animator.SetFloat(VERTICAL_FORCE, rigidBody.velocity.y);
     }
 
     void Jump() {
